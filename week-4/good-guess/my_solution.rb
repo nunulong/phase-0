68ec0d4fbbfbcd5a3f3869103b3bb099ae2124fd
nunulong <felix.wang.1026@gmail.com>
2015-11-15 22:47:06 -0500

    4.5 Flow Control Peer Pair Challenge
    - Pair with three challenges
    
    - Find solutions
    
    - Pass all the test cases

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,15 @@
+# Good Guess
+
+# I worked on this challenge [by myself, with: ].
+
+# Myself
+
+# Your Solution Below
+
+def good_guess?(input_num)
+  if input_num == 42
+    return true
+  else
+    return false
+  end
+end
